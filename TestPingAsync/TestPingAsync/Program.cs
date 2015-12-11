695e12f20936542fa659d7ed11d008d1cf478036
Sid0815 <sid0815@arcor.de>
2015-12-11 19:41:35 +0100

    TestAsyncPing
    TestAsyncPing added

---
+++ TestPingAsync/TestPingAsync/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace TestPingAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public List<IPAddress> GetNetworks()
+        {
+            Console.WriteLine("Database.GetNetworks called");
+            try
+            {
+                FileStream fs = new FileStream("..\\..\\networks.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string read;
+                List<IPAddress> list_networks = new List<IPAddress>();
+                while (sr.Peek() != -1)
+                {
+                    read = sr.ReadLine();
+                    list_networks.Add(IPAddress.Parse(read));
+                }
+                sr.Close();
+                return list_networks;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error reading File: {0}", e.Message);
+                return null;
+            }
+        }
+    }
+}
